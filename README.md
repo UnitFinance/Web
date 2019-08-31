bc16c2a1b767979d859517ebb070c682b2249673
Q <questappsza@gmail.com>
2019-08-31 17:51:00 +0200

    Init

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web
-Unit Finance company website
---
+++ precache-manifest.b1b0fae6113734441e2f7622d1514880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd7f68f917d64d66085ea675f4b5252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba109b123786dcf7b9e8",
+    "url": "/static/css/main.726e7ccd.chunk.css"
+  },
+  {
+    "revision": "64e29c04130f528490be",
+    "url": "/static/js/2.13203d97.chunk.js"
+  },
+  {
+    "revision": "ba109b123786dcf7b9e8",
+    "url": "/static/js/main.efc243b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
